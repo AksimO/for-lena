e6063bb7fb6a78149d624d62fa3a2ce8f0d653fd
Maksim <m.romanenko@snaswap.eu>
2023-03-09 10:37:03 +0100

    Hello world

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import { App } from "./App";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<App />, rootElement);
